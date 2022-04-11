a2c88411bba034274c05f125c6ee60323b8e6a62
DrSavauG <46158444+DrSavauG@users.noreply.github.com>
2022-04-11 16:38:33 +0300

    feat: add edit menu

--- services/token-service.js
+++ services/token-service.js
@@ -3,7 +3,7 @@ import tokenModel from '../models/token-model';
 
 class TokenService {
   generateTokens(payload) {
-    const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, { expiresIn: '5m' });
+    const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, { expiresIn: '1d' });
     const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, { expiresIn: '3d' });
     return {
       accessToken,
